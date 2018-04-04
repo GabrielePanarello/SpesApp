d04c73d24a6e966e4f436b0285d6cc16a3b30bae
Gabriele Moia <gabriele.moia@icloud.com>
2018-04-04 10:41:47 +0200

    Creazione dell'header component prima del component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderBeforeLoginComponent } from './Components/header-before-login/header-before-login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBeforeLoginComponent
   ],
   imports: [
     BrowserModule
