a72b5c5e39ae359ec2d60828167a5a774bb691d6
GabrielePanarello <gbrlpanarello@gmail.com>
2018-04-04 10:41:17 +0200

    Init HeaderAfterLogin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderAfterLoginComponent } from './Components/header-after-login/header-after-login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderAfterLoginComponent
   ],
   imports: [
     BrowserModule
