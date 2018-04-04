6ac256f677bec1811b8732305f0cddd11fc12ae9
GabrielePanarello <gbrlpanarello@gmail.com>
2018-04-04 17:35:11 +0200

    Footer Completato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderAfterLoginComponent } from './Components/header-after-login/header-after-login.component';
 import { HomeComponent } from './Components/home/home.component';
 import { HeaderBeforeLoginComponent } from './Components/header-before-login/header-before-login.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { HeaderBeforeLoginComponent } from './Components/header-before-login/hea
     AppComponent,
     HeaderAfterLoginComponent,
     HomeComponent,
-    HeaderBeforeLoginComponent
+    HeaderBeforeLoginComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
