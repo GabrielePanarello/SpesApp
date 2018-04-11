279d6d4fb78135ba139dd0c04628c03678bf69f8
GabrieleMoia <gabriele.moia@icloud.com>
2018-04-11 20:01:20 +0200

    Aggiunta del campo description nella list e Mock

--- src/app/Mock/in-memory-data-service.ts
+++ src/app/Mock/in-memory-data-service.ts
@@ -8,14 +8,14 @@ export class InMemoryDataService implements InMemoryDbService {
 
         const lists = [
             {
-                id: 1, name: "Spesa", img: "-", userId: 1, product : [
+                id: 1, name: "Spesa", img: "-", userId: 1, description:"spesa del sabato", product : [
                       {id: 1, img: "-", name: "Pasta", dose: "100gr", quantity: "2", took: false },
                       {id: 2, img: "-", name: "Paste", dose: "100gr", quantity: "2", took: false }
                     ]
                      
             },
              {
-                 id: 2, name: "Pasquetta", img: "-", userId: 2, product: [
+                 id: 2, name: "Pasquetta", img: "-", userId: 2, description:"spesa di pasquetta", product: [
                      { id: 2, img: "-", name: "Pasta", dose: "100gr", quantity: "2", took: false }
                  ] 
              }
--- src/app/beans/list.ts
+++ src/app/beans/list.ts
@@ -6,12 +6,14 @@ export class List{
     img: string;
     product: Product[];
     userId: number;
+    description:String;
 
-    constructor(id: number, name: string, img: string, product: Product[],userId: number){
+    constructor(id: number, name: string, img: string, product: Product[],userId: number, description:String){
         this.id = id;
         this.name = name;
         this.img = img;
         this.product = product;
         this.userId = userId;
+        this.description=description;
     }
 }
